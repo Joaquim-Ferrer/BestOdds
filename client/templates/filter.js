2eacf02b7c9386f06b9798421794373dc472ee4f
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-02-21 13:44:23 +0000

    starts filtering with checkboxes

--- client/templates/filter.js
+++ client/templates/filter.js
@@ -1,6 +1,6 @@
 Template.filter.helpers({
 	options: function(){
-		return ["World","Portugal", "USA", "Germany", "UK", "Italy"];
+		return ["Portugal", "USA", "Germany", "UK", "Italy"];
 	},
 	selection: function() {
 		filter = Session.get("filter");
@@ -14,31 +14,31 @@ Template.filter.events({
 	"change .add_filter": function(event) {
 		var country_added = event.target.value;
 		filter = Session.get("filter");
-		
-		if(country_added === "World") {
-				filter["country"] = {"$in": ["Portugal", "USA", "Germany", "UK", "Italy"]};
-				Session.set("filter", filter);
-				return;
-		}
+		console.log(event);
+		/*if(country_added === "All" && event.target.checked) {
+			fo
+		}*/
 		if(filter["country"]) {
 			countries_in_filter = filter["country"]["$in"]
-			if(countries_in_filter.indexOf(country_added)<0) {
+			if(event.target.checked) {
 				countries_in_filter.push(country_added);
 				filter["country"] = {"$in": countries_in_filter};
-				Session.set("filter", filter);
 			}
-			return;
+			else {
+				index = filter["country"]["$in"].indexOf(event.target.value);
+				if(index > -1) {
+					filter["country"]["$in"].splice(index, 1);
+				}
+			}
 		}
 		else {
 			filter["country"] = { "$in": country_added } 
-			Session.set("filter", filter);
 		}
+		Session.set("filter", filter);
 	},
 	"click .tag": function(event) {
-		console.log(event)
 		filter = Session.get("filter");
 		index = filter["country"]["$in"].indexOf(event.target.textContent);
-		console.log(index);
 		if(index > -1) {
 			 filter["country"]["$in"].splice(index, 1);
 		}
