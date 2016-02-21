60b6893eb3dec3819aa872b2996141aa7a68f0d4
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-02-21 11:20:21 +0000

    Removes more dummy data

--- bestOdds/server/publications.js
+++ bestOdds/server/publications.js
@@ -1,9 +1,3 @@
-draws = {"numbers": [1,2,3,5], "prize": 90000}
-
-if (LotteryDraws.find({}).count() === 0) {
-        LotteryDraws.insert(draws);
-};
-
 Meteor.publish("recentDrawings", function(lim)
 {
 	return LotteryDraws.find({}, {limit: lim});
