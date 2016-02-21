e836bb4cff250a7efea2f989eee5d8da6ad3a425
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-02-21 12:29:37 +0000

    adds details to the drawing_presentations templates

--- client/templates/drawing_presentation.js
+++ client/templates/drawing_presentation.js
@@ -1,5 +1,18 @@
 Template.drawingPresentation.helpers({
 	numbers: function() {
 		return this.numbers.toString();
-	}
+	},
+	date: function() {
+		return this.display_date + ", " + this.weekday;
+	},
+	has_next_prize: function() {
+		return !!this.next_prize;
+	},
+	has_rolover: function() {
+		console.log("sdas" + this.next_rollover)
+		if(this.next_rollover >= 0){
+			return 1;
+		}
+		return 0;
+	},
 });
