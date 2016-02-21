0d035f51f1279348c43fdfee9f6d70d49f3b0f73
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-02-21 11:18:09 +0000

    Removes automatic insertion of dummy data

--- bestOdds/server/publications.js
+++ bestOdds/server/publications.js
@@ -6,11 +6,6 @@ if (LotteryDraws.find({}).count() === 0) {
 
 Meteor.publish("recentDrawings", function(lim)
 {
-	LotteryDraws.remove({});
-	draws = {"normal_balls": [1,2,3,5],"special_balls": [1,2], "prize": 90000, "next_prize":1000, "game":"PowerBall", "country": ["USA"], "winner": 0, "date": "19 de Setembro", "next_date": "20 de Setembro"};
-	if (LotteryDraws.find({}).count() === 0) {
-        LotteryDraws.insert(draws);
-	};
 	return LotteryDraws.find({}, {limit: lim});
 });
 
